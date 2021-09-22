14f8215fcf9f8821d978f585b70ce5dc4a9cded0
Carlos Melegrito <caltech511@gmail.com>
2021-09-22 13:47:28 -0300

    service section completed

--- src/components/section-header.js
+++ src/components/section-header.js
@@ -3,14 +3,26 @@ import { Box, Text, Heading } from 'theme-ui';
 
 export default function SectionHeader({ title, slogan, isWhite }) {
   return (
-    <Box sx={{variant: 'sectionHeader'}}>
+    <Box sx={{ variant: "sectionHeader" }}>
       <Text
         as="p"
-        sx
+        sx={{
+          variant: "sectionHeader.subTitle",
+          color: isWhite ? "white" : "primary",
+          opacity: isWhite ? 0.7 : 1,
+        }}
       >
-
+        {slogan}
       </Text>
-      
-      </Box>
+      <Heading
+        as="h2"
+        sx={{
+          variant: "sectionHeader.title",
+          color: isWhite ? "white" : "heading",
+        }}
+      >
+        {title}
+      </Heading>
+    </Box>
   );
 }
